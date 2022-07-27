33e7dcfa9f24d19dbbcfa235cb47e5d8f28b552a
Alexa Coffman <coffman.alexa@gmail.com>
2022-07-27 13:21:26 -0700

    adds file structure

---
+++ src/main/java/com/kpme/kpme/KpmeApplication.java
@@ -0,0 +1,13 @@
+package com.kpme.kpme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KpmeApplication.class, args);
+	}
+
+}
