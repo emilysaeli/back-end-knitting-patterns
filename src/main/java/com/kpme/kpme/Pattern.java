dcdf7c1fa5fb42ad6a01f4c3aef3b7d3e190e916
Alexa Coffman <coffman.alexa@gmail.com>
2022-08-02 15:21:39 -0700

    Added CORS dependency and decorator

---
+++ src/main/java/com/kpme/kpme/Pattern.java
@@ -0,0 +1,13 @@
+package com.kpme.kpme;
+
+public class Pattern {
+    private String inputPattern;
+
+    public void setInputPattern(){
+        this.inputPattern = inputPattern;
+    }
+    public String getInputPattern() {
+        return inputPattern;
+
+    }
+}
--- src/main/java/com/kpme/kpme/PatternHandlerController.java
+++ src/main/java/com/kpme/kpme/PatternHandlerController.java
@@ -11,6 +11,7 @@ public class PatternHandlerController {
 
     private static final String template = "Here is your pattern: %s!";
 
+    @CrossOrigin(origins = "*")
     @PostMapping("")
     public PatternHandler createPattern(@RequestBody String inputPattern) {
         return new PatternHandler(String.format(template, inputPattern));
