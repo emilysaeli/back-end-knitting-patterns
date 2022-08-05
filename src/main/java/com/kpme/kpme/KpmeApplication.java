5b9f249a09756d53bfd1f9296cc2ded0227a92f4
Alexa Coffman <coffman.alexa@gmail.com>
2022-08-05 12:05:56 -0700

    refactor, moved methods around to more appropriate classes, added comments, removed unnecessary lines

--- src/main/java/com/kpme/kpme/KpmeApplication.java
+++ src/main/java/com/kpme/kpme/KpmeApplication.java
@@ -3,11 +3,6 @@ package com.kpme.kpme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Arrays;
-
-import static com.kpme.kpme.Row.generateRow;
-import static com.kpme.kpme.Row.parsePattern;
-
 @SpringBootApplication
 public class KpmeApplication {
 
--- src/main/java/com/kpme/kpme/PatternHandlerController.java
+++ src/main/java/com/kpme/kpme/PatternHandlerController.java
@@ -1,16 +1,11 @@
 package com.kpme.kpme;
 
-
 import org.springframework.web.bind.annotation.*;
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 
 @RestController
 public class PatternHandlerController {
-
-//    private static final String template = "Here is your pattern: %s!";
 
     @CrossOrigin(origins = "*")
     @PostMapping("")
